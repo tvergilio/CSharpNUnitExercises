a0ee848c5a067b20c0e279b68591393f3777d61a
tvergilio <thalita.vergilio@xdesign.com>
2024-05-22 15:09:36 +0100

    Added broken code implementation

--- CSharpNUnitExercises/BrokenCode.cs
+++ CSharpNUnitExercises/BrokenCode.cs
@@ -1,54 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace CSharpNUnitExercises
 {
     public class BrokenCode
     {
         public int SumNumbers(IEnumerable<int> numbers)
         {
-            var result = 0;
-            foreach (var number in numbers)
-            {
-                result = number;
-            }
-            return result;
+            return numbers.Sum();
         }
 
         public IEnumerable<string> ReverseStrings(IEnumerable<string> input)
         {
-            var result = new List<string>();
-
-            foreach (string s in input)
-            {
-                if (s == null)
-                {
-                    result.Add(null);
-                }
-                else
-                {
-                    var array = s.ToCharArray();
-                    Array.Reverse(array);
-                    result.Add("s");
-                }
-
-            }
-
-            return result.ToArray();
+            return input.Select(s => s == null ? null : new string(s.Reverse().ToArray()));
         }
 
-        public IEnumerable<string> AltReverseStrings(string[] input)
-        {
-            var result = new List<string>();
-
-            foreach (string s in input)
-            {
-                var array = s.ToCharArray();
-                result.Add(new string(array));
-            }
-
-            return result;
-        }
+       public IEnumerable<string> AltReverseStrings(string[] input)
+       {
+           return input.Select(s => s == null ? null : new string(s.Reverse().ToArray()));
+       }
     }
 }
